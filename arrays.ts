de78ab723edc14b9a3b20e1812716e288495c9c4
Umaralikhon <umaralikhon@gmail.com>
2023-11-01 22:36:54 +0500

    First commit

---
+++ arrays.ts
@@ -0,0 +1,2 @@
+let simpleArray: number[] = [1, 2, 3];
+let generaricArray: Array<number> = [1, 2, 3];
---
+++ types.ts
@@ -0,0 +1,7 @@
+let num: number = 5;
+let num2: number = 3.4;
+let str: string = "Veronica";
+let str2: string = `My name is ${str}`;
+let greeting = (): void => {
+    alert(`Hello ${str}`);
+}
